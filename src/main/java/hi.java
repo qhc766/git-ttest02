a1f830abb7cd9a36f5b01ee7bea279335ded161f
zhibo <zhibo766@126.com>
2023-02-09 03:51:57 +0800

    hot-fix commit

--- src/main/java/hi.java
+++ src/main/java/hi.java
@@ -2,5 +2,6 @@ public class hi {
     public static void main(String[] args) {
         System.out.println("long time no see~");
         System.out.println("i miss u~");
+        System.out.println("hot coming~");
     }
 }
